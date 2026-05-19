2df120f75768b19c979a2033b3a0555344033e1a
Noarrrrr <noah.patton1225@gmail.com>
2026-05-19 13:59:41 -0700

    WIP home page added

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-from rich import print
-from rich.console import Console
-from rich.layout import Layout
-from rich.panel import Panel
-from rich.table import Table
-from rich.live import Live
-from rich.text import Text
+import sys
+#from scramble import generate_scramble
+#from cube import solved_cube
+from splashes import welcome_splash
 
-import time
-import random
 
+def main():
+    home()
+
+def home():
+    print("\033[2J\033[H", end="")
+    print(welcome_splash)
+    selection("home")
+
+def scramble_page():
+    pass
+
+def cross_puzzle():
+    pass
+
+def selection(page):
+    if page == "home":
+        choice = input("1. Generate Scramble\n2. Cross Puzzle\n3. Exit\n")
+        if choice == "1":
+            scramble_page()
+        elif choice == "2":
+            cross_puzzle()
+        elif choice == "3":
+            print("\033[2J\033[H", end="")
+            sys.exit(0)
+
+main()
