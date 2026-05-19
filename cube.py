06843944319e832f3d349405386d7f915323cbfe
Noarrrrr <noah.patton1225@gmail.com>
2026-05-19 00:59:23 -0700

    cube agent no longer crashes and can find the white cross relatively efficiently

--- cube.py
+++ cube.py
@@ -1,75 +1,57 @@
-from scramble import generate_scramble
+import copy
+from enum import Enum
+import time
+import cProfile
+from scramble import generate_scramble, generate_bf_moves
 
-solved_cube = {
-    "top": {1: "⬜", 2: "⬜", 3: "⬜", 4: "⬜", 5: "⬜", 6: "⬜", 7: "⬜", 8: "⬜", 9: "⬜"},
-    "bottom": {1: "🟨", 2: "🟨", 3: "🟨", 4: "🟨", 5: "🟨", 6: "🟨", 7: "🟨", 8: "🟨", 9: "🟨"},
-    "front": {1: "🟩", 2: "🟩", 3: "🟩", 4: "🟩", 5: "🟩", 6: "🟩", 7: "🟩", 8: "🟩", 9: "🟩"},
-    "back": {1: "🟦", 2: "🟦", 3: "🟦", 4: "🟦", 5: "🟦", 6: "🟦", 7: "🟦", 8: "🟦", 9: "🟦"},
-    "right": {1: "🟥", 2: "🟥", 3: "🟥", 4: "🟥", 5: "🟥", 6: "🟥", 7: "🟥", 8: "🟥", 9: "🟥"},
-    "left": {1: "🟧", 2: "🟧", 3: "🟧", 4: "🟧", 5: "🟧", 6: "🟧", 7: "🟧", 8: "🟧", 9: "🟧"},
-}
+solved_cube = [
+    0,0,0,0,0,0,0,0,0,  #bottom/white
+    1,1,1,1,1,1,1,1,1,  #top/yellow
+    2,2,2,2,2,2,2,2,2,  #left/red
+    3,3,3,3,3,3,3,3,3,  #right/orange
+    4,4,4,4,4,4,4,4,4,  #front/green
+    5,5,5,5,5,5,5,5,5,  #back/blue
+]
+
+stkrs = ["⬜", "🟨", "🟥", "🟧", "🟩", "🟦"]
 
 def print_cube(cube):
-    for stkr in range(1, 8, 3):
-        print(f"      {cube["top"][stkr+0]}{cube["top"][stkr+1]}{cube["top"][stkr+2]}")
-    for stkr in range(1, 8, 3):
-        print(f"{cube["left"][stkr+0]}{cube["left"][stkr+1]}{cube["left"][stkr+2]}{cube["front"][stkr+0]}{cube["front"][stkr+1]}{cube["front"][stkr+2]}{cube["right"][stkr+0]}{cube["right"][stkr+1]}{cube["right"][stkr+2]}{cube["back"][stkr+0]}{cube["back"][stkr+1]}{cube["back"][stkr+2]}")
-    for stkr in range(1, 8, 3):
-        print(f"      {cube["bottom"][stkr+0]}{cube["bottom"][stkr+1]}{cube["bottom"][stkr+2]}")
+    print(f"""      {stkrs[cube[9]]}{stkrs[cube[10]]}{stkrs[cube[11]]}
+      {stkrs[cube[12]]}{stkrs[cube[13]]}{stkrs[cube[14]]}
+      {stkrs[cube[15]]}{stkrs[cube[16]]}{stkrs[cube[17]]}
+{stkrs[cube[18]]}{stkrs[cube[19]]}{stkrs[cube[20]]}{stkrs[cube[36]]}{stkrs[cube[37]]}{stkrs[cube[38]]}{stkrs[cube[27]]}{stkrs[cube[28]]}{stkrs[cube[29]]}{stkrs[cube[45]]}{stkrs[cube[46]]}{stkrs[cube[47]]}
+{stkrs[cube[21]]}{stkrs[cube[22]]}{stkrs[cube[23]]}{stkrs[cube[39]]}{stkrs[cube[40]]}{stkrs[cube[41]]}{stkrs[cube[30]]}{stkrs[cube[31]]}{stkrs[cube[32]]}{stkrs[cube[48]]}{stkrs[cube[49]]}{stkrs[cube[50]]}
+{stkrs[cube[24]]}{stkrs[cube[25]]}{stkrs[cube[26]]}{stkrs[cube[42]]}{stkrs[cube[43]]}{stkrs[cube[44]]}{stkrs[cube[33]]}{stkrs[cube[34]]}{stkrs[cube[35]]}{stkrs[cube[51]]}{stkrs[cube[52]]}{stkrs[cube[53]]}
+      {stkrs[cube[0]]}{stkrs[cube[1]]}{stkrs[cube[2]]}
+      {stkrs[cube[3]]}{stkrs[cube[4]]}{stkrs[cube[5]]}
+      {stkrs[cube[6]]}{stkrs[cube[7]]}{stkrs[cube[8]]}""")
+    
 
-
-def cycle_stickers(cube, stickers, side=False):
-    if side == True:
-        temp_stkrs = stickers[:3]
-        new_placement = stickers[3:] + temp_stkrs
-    else:
-        temp_stkrs = stickers[:2]
-        new_placement = stickers[2:] + temp_stkrs
-
-    new_placement_raw_data = []
-    for i in range(0, len(stickers)):
-        new_placement_raw_data.append(cube[new_placement[i][0]][new_placement[i][1]])
-
-    for i in range(0, len(stickers)):
-        cube[stickers[i][0]][stickers[i][1]] = new_placement_raw_data[i]
-    return cube
 
     
 def move_top(cube):
-    side_stickers = [("left", 1), ("left", 2), ("left", 3), ("front", 1), ("front", 2), ("front", 3), ("right", 1), ("right", 2), ("right", 3), ("back", 1), ("back", 2), ("back", 3)]
-    cycle_stickers(cube, side_stickers, side=True)
-    top_stickers = [("top", 4), ("top", 7), ("top", 8), ("top", 9), ("top", 6), ("top", 3), ("top", 2), ("top", 1)]
-    cycle_stickers(cube, top_stickers)
+    cube[9], cube[10], cube[11], cube[14], cube[17], cube[16], cube[15], cube[12] = cube[15], cube[12], cube[9], cube[10], cube[11], cube[14], cube[17], cube[16]
+    cube[47], cube[46], cube[45], cube[29], cube[28], cube[27], cube[38], cube[37], cube[36], cube[20], cube[19], cube[18] = cube[20], cube[19], cube[18], cube[47], cube[46], cube[45], cube[29], cube[28], cube[27], cube[38], cube[37], cube[36]
 
 def move_bottom(cube):
-    side_stickers = [("back", 9), ("back", 8), ("back", 7), ("right", 9), ("right", 8), ("right", 7), ("front", 9), ("front", 8), ("front", 7), ("left", 9), ("left", 8), ("left", 7)]
-    cycle_stickers(cube, side_stickers, side=True)
-    top_stickers = [("bottom", 4), ("bottom", 7), ("bottom", 8), ("bottom", 9), ("bottom", 6), ("bottom", 3), ("bottom", 2), ("bottom", 1)]
-    cycle_stickers(cube, top_stickers)
+    cube[0], cube[1], cube[2], cube[5], cube[8], cube[7], cube[6], cube[3] = cube[6], cube[3], cube[0], cube[1], cube[2], cube[5], cube[8], cube[7]
+    cube[42], cube[43], cube[44], cube[33], cube[34], cube[35], cube[51], cube[52], cube[53], cube[24], cube[25], cube[26] = cube[24], cube[25], cube[26], cube[42], cube[43], cube[44], cube[33], cube[34], cube[35], cube[51], cube[52], cube[53]
 
 def move_right(cube):
-    side_stickers = [("top", 3), ("top", 6), ("top", 9), ("front", 3), ("front", 6), ("front", 9), ("bottom", 3), ("bottom", 6), ("bottom", 9), ("back", 7), ("back", 4), ("back", 1)]
-    cycle_stickers(cube, side_stickers, side=True)
-    top_stickers = [("right", 4), ("right", 7), ("right", 8), ("right", 9), ("right", 6), ("right", 3), ("right", 2), ("right", 1)]
-    cycle_stickers(cube, top_stickers)
+    cube[27], cube[28], cube[29], cube[32], cube[35], cube[34], cube[33], cube[30] = cube[33], cube[30], cube[27], cube[28], cube[29], cube[32], cube[35], cube[34]
+    cube[17], cube[14], cube[11], cube[45], cube[48], cube[51], cube[8], cube[5], cube[2], cube[44], cube[41], cube[38] = cube[44], cube[41], cube[38], cube[17], cube[14], cube[11], cube[45], cube[48], cube[51], cube[8], cube[5], cube[2]
 
 def move_left(cube):
-    side_stickers = [("bottom", 7), ("bottom", 4), ("bottom", 1), ("front", 7), ("front", 4), ("front", 1), ("top", 7), ("top", 4), ("top", 1), ("back", 3), ("back", 6), ("back", 9)]
-    cycle_stickers(cube, side_stickers, side=True)
-    top_stickers = [("left", 4), ("left", 7), ("left", 8), ("left", 9), ("left", 6), ("left", 3), ("left", 2), ("left", 1)]
-    cycle_stickers(cube, top_stickers)
+    cube[18], cube[19], cube[20], cube[23], cube[26], cube[25], cube[24], cube[21] = cube[24], cube[21], cube[18], cube[19], cube[20], cube[23], cube[26], cube[25]
+    cube[9], cube[12], cube[15], cube[36], cube[39], cube[42], cube[0], cube[3], cube[6], cube[53], cube[50], cube[47] = cube[53], cube[50], cube[47], cube[9], cube[12], cube[15], cube[36], cube[39], cube[42], cube[0], cube[3], cube[6]
 
 def move_front(cube):
-    side_stickers = [("bottom", 1), ("bottom", 2), ("bottom", 3), ("right", 7), ("right", 4), ("right", 1), ("top", 9), ("top", 8), ("top", 7), ("left", 3), ("left", 6), ("left", 9)]
-    cycle_stickers(cube, side_stickers, side=True)
-    top_stickers = [("front", 4), ("front", 7), ("front", 8), ("front", 9), ("front", 6), ("front", 3), ("front", 2), ("front", 1)]
-    cycle_stickers(cube, top_stickers)
+    cube[36], cube[37], cube[38], cube[41], cube[44], cube[43], cube[42], cube[39] = cube[42], cube[39], cube[36], cube[37], cube[38], cube[41], cube[44], cube[43]
+    cube[15], cube[16], cube[17], cube[27], cube[30], cube[33], cube[2], cube[1], cube[0], cube[26], cube[23], cube[20] = cube[26], cube[23], cube[20], cube[15], cube[16], cube[17], cube[27], cube[30], cube[33], cube[2], cube[1], cube[0]
 
 def move_back(cube):
-    side_stickers = [("left", 7), ("left", 4), ("left", 1), ("top", 1), ("top", 2), ("top", 3), ("right", 3), ("right", 6), ("right", 9), ("bottom", 9), ("bottom", 8), ("bottom", 7)]
-    cycle_stickers(cube, side_stickers, side=True)
-    top_stickers = [("back", 4), ("back", 7), ("back", 8), ("back", 9), ("back", 6), ("back", 3), ("back", 2), ("back", 1)]
-    cycle_stickers(cube, top_stickers)
+    cube[45], cube[46], cube[47], cube[50], cube[53], cube[52], cube[51], cube[48] = cube[51], cube[48], cube[45], cube[46], cube[47], cube[50], cube[53], cube[52]
+    cube[11], cube[10], cube[9], cube[18], cube[21], cube[24], cube[6], cube[7], cube[8], cube[35], cube[32], cube[29] = cube[35], cube[32], cube[29], cube[11], cube[10], cube[9], cube[18], cube[21], cube[24], cube[6], cube[7], cube[8]
 
 
 def apply_moves(cube, moves):
@@ -128,13 +110,114 @@ def apply_moves(cube, moves):
             for _ in range(2):
                 move_back(cube)
 
+def reverse_moves(cube, moves):
+    for move in reversed(moves):
+        if move == "R'":
+            move_right(cube)
+        elif move == "R":
+            for _ in range(3):
+                move_right(cube)
+        elif move == "R2":
+            for _ in range(2):
+                move_right(cube)
+        
+        elif move == "L'":
+            move_left(cube)
+        elif move == "L":
+            for _ in range(3):
+                move_left(cube)
+        elif move == "L2":
+            for _ in range(2):
+                move_left(cube)
+
+        elif move == "U'":
+            move_top(cube)
+        elif move == "U":
+            for _ in range(3):
+                move_top(cube)
+        elif move == "U2":
+            for _ in range(2):
+                move_top(cube)
+
+        elif move == "D'":
+            move_bottom(cube)
+        elif move == "D":
+            for _ in range(3):
+                move_bottom(cube)
+        elif move == "D2":
+            for _ in range(2):
+                move_bottom(cube)
+        
+        elif move == "F'":
+            move_front(cube)
+        elif move == "F":
+            for _ in range(3):
+                move_front(cube)
+        elif move == "F2":
+            for _ in range(2):
+                move_front(cube)
+
+        elif move == "B'":
+            move_back(cube)
+        elif move == "B":
+            for _ in range(3):
+                move_back(cube)
+        elif move == "B2":
+            for _ in range(2):
+                move_back(cube)
+
+
+def find_cross(cube):
+    solution_found = False
+    solution_count = 0
+    for depth in range(1, 6):
+        print(f"Searching {depth} moves deep")
+        possible_solutions = generate_bf_moves(depth)
+        for moves in possible_solutions:
+            if check_for_cross(cube, moves):
+                if solution_count < 3:
+                    print(f"Found Solution!\n{" ".join(moves)}")
+                    apply_moves(cube, moves)
+                    print_cube(cube)
+                    reverse_moves(cube, moves)
+                    solution_count += 1
+                    solution_found = True
+                else:
+                    return
+        if solution_found == True:
+            return
+
+        
+    print("Something's amiss")
+
+def check_for_cross(cube, moves):
+    apply_moves(cube, moves)
+    if (
+        cube[1] == 0 and
+        cube[5] == 0 and
+        cube[7] == 0 and
+        cube[3] == 0 and
+        cube[43] == 4 and
+        cube[34] == 3 and
+        cube[52] == 5 and
+        cube[25] == 2
+    ):
+        reverse_moves(cube, moves)
+        return True
+    reverse_moves(cube, moves)
+    return False
+
+
 
 def test():
     cube = solved_cube
     scramble = generate_scramble(17)
-    print_cube(cube)
+    #print_cube(cube)
+    #print_cube(cube)
     print(f"Applying scramble:\n{" ".join(scramble)}")
     apply_moves(cube, scramble)
     print_cube(cube)
+    print("Searching for White Cross...")
+    cProfile.run(f"find_cross({cube})")
 
 test()
--- scramble.py
+++ scramble.py
@@ -1,25 +1,32 @@
 import random
 
-notation = [
+scr_notation = [
     ["R","R'","R2","R2","L","L'","L2","L2"],
     ["U","U'","U2","U2","D","D'","D2","D2"],
     ["F","F'","F2","F2","B","B'","B2","B2"]
 ]
 
-def generate_scramble(move_count):
-    available_moves = notation
+notation = [
+    ["R","R'","R2","L","L'","L2"],
+    ["U","U'","U2","D","D'","D2"],
+    ["F","F'","F2","B","B'","B2"]
+]
+
+
+def generate_scramble(move_count): #producing ABA patterns
+    available_moves = scr_notation
     scramble = []
     last_move = "nothin"
     last_move_axis = "nothin"
     axis_uses = 0
     for i in range(move_count):
-        axis = random.choice(notation)
+        axis = random.choice(scr_notation)
         move = random.choice(axis)
         while move[0] == last_move[0]:
             move = random.choice(axis)
         scramble.append(move)
         if len(available_moves) < 3:
-            available_moves = notation
+            available_moves = scr_notation
         elif axis == last_move_axis:
             axis_uses += 1
             if axis_uses == 2:
@@ -28,4 +35,34 @@ def generate_scramble(move_count):
         last_move_axis = axis
     return scramble
 
-#print(" ".join(generate_scramble(17))
+def generate_bf_moves(move_depth, movesets=None):
+    if move_depth == 0:
+        return movesets
+    
+    if not movesets:
+        initial_movesets = []
+        for axis in notation:
+            for move in axis:
+                initial_movesets.append([move])
+        return generate_bf_moves(move_depth - 1, initial_movesets)
+    
+    else:
+        new_movesets = []
+        for moveset in movesets:
+            for axis in notation:
+                if len(moveset) > 1 and (moveset[-1] in axis and moveset[-2] in axis):
+                    continue
+                elif moveset[-1] in axis:
+                    for move in axis:
+                        if move[0] != moveset[-1][0]:
+                            new_movesets.append(moveset + [move])
+                else:
+                    for move in axis:
+                        new_movesets.append(moveset + [move])
+        return generate_bf_moves(move_depth - 1, new_movesets)
+
+
+
+#print(len(generate_bf_moves(5)))
+
+#print(" ".join(generate_scramble(17)))
